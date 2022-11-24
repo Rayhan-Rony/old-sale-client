b4acb50dab048ae28ebc90b6a4ade1a92dc2c841
Rayhan-Rony <jobayerahmedrony@gmail.com>
2022-11-24 22:59:06 +0600

    added category

--- src/Pages/Home/Categories/Categories.js
+++ src/Pages/Home/Categories/Categories.js
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios'
 
 const Categories = () => {
+    const [categories, setCategories] = useState([])
+
+    // data load using axios
+
+    useEffect(() => {
+        axios.get('http://localhost:5000/categories')
+            .then(data => {
+                // console.log(data.data)
+                const phoneCategories = data.data
+                setCategories(phoneCategories)
+            })
+    }, [])
     return (
-        <div className="card card-compact w-96 bg-base-100 shadow-xl">
-            <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
-            <div className="card-body">
-                <h2 className="card-title">Shoes!</h2>
-                <p>If a dog chews shoes whose shoes does he choose?</p>
-                <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Buy Now</button>
-                </div>
+        <div className='my-20'>
+            <h2 className='text-5xl text-center font-extrabold mb-20'>Categories</h2>
+            <div className='grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-5 '>
+                {
+                    categories.map(category => <div key={category._id} className="card card-compact  bg-base-100 shadow-xl">
+                        <figure><img className='w-1/2 h-40' src={category.img} alt="Shoes" /></figure>
+                        <div className="card-body">
+                            <h2 className="card-title">{category.name}</h2>
+                            <div className="card-actions justify-end">
+                                <button className="btn btn-outline">See all phones</button>
+                            </div>
+                        </div>
+                    </div>)
+                }
             </div>
         </div>
     );
