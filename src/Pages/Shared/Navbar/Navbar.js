bff3cd25835160aa92c6a62e26563f7dfffc80d7
Rayhan-Rony <jobayerahmedrony@gmail.com>
2022-11-27 04:28:33 +0600

    added user role based route changed functionality

--- src/Pages/Shared/Navbar/Navbar.js
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -1,10 +1,23 @@
+import { useQuery } from '@tanstack/react-query';
 import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { AuthContext } from '../../../Context/AuthProvider';
 
 const Navbar = () => {
     const { logOut, user } = useContext(AuthContext)
-    console.log(user)
+    console.log(user?.email)
+    const { data: users = [] } = useQuery({
+        queryKey: ['users', user],
+        queryFn: () => fetch(`http://localhost:5000/users?email=${user?.email}`)
+            .then(res => res.json())
+    })
+    // const { data: products = [] } = useQuery({
+    //     queryKey: ['products', id],
+    //     queryFn: () => fetch(url)
+    //         .then(res => res.json())
+
+    // })
+    // console.log(users[0].role)
     return (
         <div className="navbar bg-gray-200 rounded-sm">
             <div className="flex-1">
@@ -17,7 +30,18 @@ const Navbar = () => {
                     {
                         user?.uid ?
                             <>
-                                <li><Link to='/dashboard'>Dashboard</Link></li>
+
+                                {
+                                    users[0]?.role === 'seller' &&
+                                    <li><Link to='/dashboard'>Add A Product</Link></li>
+
+                                }
+                                {
+                                    users[0]?.role === 'user' &&
+                                    <li><Link to='/dashboard'>My Orders</Link></li>
+
+                                }
+
                                 <li><Link onClick={logOut}>log Out</Link></li>
                             </>
                             : <li><Link to='/login'>Log In</Link></li>
