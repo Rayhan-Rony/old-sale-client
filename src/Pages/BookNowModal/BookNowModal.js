11d1ea309c37757051add4921943d277486981b5
Rayhan-Rony <jobayerahmedrony@gmail.com>
2022-11-28 05:13:39 +0600

    added close modal and edit soldStatus for booking

--- src/Pages/BookNowModal/BookNowModal.js
+++ src/Pages/BookNowModal/BookNowModal.js
@@ -2,14 +2,22 @@ import React, { useContext } from 'react';
 import toast from 'react-hot-toast';
 import { AuthContext } from '../../Context/AuthProvider';
 
-const BookNowModal = ({ modalproduct }) => {
+const BookNowModal = ({ setModalProduct, modalproduct }) => {
     const { user } = useContext(AuthContext)
-    // console.log(product)
+
     const handleBook = (e) => {
         e.preventDefault()
         toast.success('Added Your Booking')
-        console.log(modalproduct.name)
-
+        const id = modalproduct._id
+        console.log(id)
+        fetch(`http://localhost:5000/products/${id}`, {
+            method: 'PUT'
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+            })
+        setModalProduct(null)
     }
     return (
         <>
@@ -21,15 +29,11 @@ const BookNowModal = ({ modalproduct }) => {
                     <h3 className="text-lg font-bold">{modalproduct.name}</h3>
                     <form onSubmit={handleBook} className='grid grid-cols-1 gap-3 mt-10'>
                         <div className="badge badge-outline">Resale Price:{modalproduct.resalePrice} </div>
-                        {/* <input type="text" defaultValue={price} disabled className="input w-full input-bordered" /> */}
-                        {/* <select name='slot' className="select select-bordered w-full ">
 
-                            {slots.map((slot, i) => <option key={i} value={slot}>{slot}</option>)}
-                        </select> */}
                         <input name='name' defaultValue={user?.displayName} disabled type="text" placeholder="Your Name" className="input w-full input-bordered" />
-                        {/* <input name='email' defaultValue={user?.email} disabled type="email" placeholder="Your Email" className="input w-full input-bordered" /> */}
-                        <input name='phone' type="text" placeholder="Phone Number" className="input w-full input-bordered" />
-                        <input name='location' type="text" placeholder="Meeting Location" className="input w-full input-bordered" />
+
+                        <input name='phone' type="number" placeholder="Phone Number" className="input w-full input-bordered" required />
+                        <input name='location' type="text" placeholder="Meeting Location" className="input w-full input-bordered" required />
                         <br />
                         <input className='w-full  btn btn-primary' type="submit" value="Submit" />
                     </form>
--- src/Pages/Products/Products.js
+++ src/Pages/Products/Products.js
@@ -7,7 +7,7 @@ import SingleProduct from './SingleProduct';
 const Products = () => {
     const { id } = useParams()
     const [modalproduct, setModalProduct] = useState(null)
-    // const id = parseInt(par.id)
+
     const url = `http://localhost:5000/category/${id}`
     const { data: products = [] } = useQuery({
         queryKey: ['products', id],
@@ -15,13 +15,9 @@ const Products = () => {
             .then(res => res.json())
 
     })
-    console.log(products)
 
 
-    // const products = useLoaderData()
-    // console.log(products)
-    // console.log(par.id)
-    // console.log(typeof (id))
+
     return (
         <div className='grid gap-5 lg:grid-cols-3 md:grid-cols-2 grid-cols-1 my-40 w-11/12 mx-auto'>
             {
@@ -29,7 +25,7 @@ const Products = () => {
             }
             {
                 modalproduct &&
-                <BookNowModal modalproduct={modalproduct}></BookNowModal>
+                <BookNowModal setModalProduct={setModalProduct} modalproduct={modalproduct}></BookNowModal>
             }
         </div>
     );
