aea97261a07006769aeae6b0c4fefb6694babe1c
Rayhan-Rony <jobayerahmedrony@gmail.com>
2022-11-29 21:51:00 +0600

    added delete functionality in my products page

--- src/Pages/Dashboard/MyProducts/MyProducts.js
+++ src/Pages/Dashboard/MyProducts/MyProducts.js
@@ -1,9 +1,11 @@
 import { useQuery } from '@tanstack/react-query';
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { AuthContext } from '../../../Context/AuthProvider';
 
 const MyProducts = () => {
     const { user } = useContext(AuthContext)
+    // const [disabled, setDisabled] = useState(false)
+
     // console.log(user?.displayName)
 
 
@@ -12,27 +14,54 @@ const MyProducts = () => {
         queryFn: () => fetch(`http://localhost:5000/myProducts?email=${user?.email}`)
             .then(res => res.json())
     })
-    console.log(myproducts)
+    // console.log(myproducts)
 
 
     const handleAdvertise = (product) => {
         console.log(product)
+        const { category_id, img, location, originalPrice, resalePrice, sellerEmail, sellersName, soldStatus, time, usedTime, _id } = product
+        const id = _id
         const advertiseStatus = 'true'
-        const advertiseProduct = { advertiseStatus, ...product }
-        console.log(advertiseProduct)
+        const productForadd = { id, category_id, img, location, originalPrice, resalePrice, sellerEmail, sellersName, soldStatus, time, usedTime, advertiseStatus }
+        // const advertiseProduct = { advertiseStatus, ...product }
+
+        // console.log(advertiseProduct)
 
         fetch('http://localhost:5000/advertise', {
             method: 'POST',
             headers: {
                 'content-type': 'application/json'
             },
-            body: JSON.stringify(advertiseProduct)
+            body: JSON.stringify(productForadd)
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.acknowledged === true) {
+                    console.log('true')
+                    // setDisabled(true)
+                }
+            })
+    }
+    const handleDelete = (id) => {
+        console.log(id)
+        fetch(`http://localhost:5000/advertise/${id}`, {
+            method: 'DELETE',
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+            })
+
+        fetch(`http://localhost:5000/products/${id}`, {
+            method: 'DELETE',
         })
             .then(res => res.json())
             .then(data => {
                 console.log(data)
             })
     }
+
+    // console.log(disabled)
     return (
         <div className="overflow-x-auto min-h-screen">
             <table className="table w-full">
@@ -59,15 +88,20 @@ const MyProducts = () => {
                             <td>${myProduct.resalePrice}</td>
                             <td>{myProduct.soldStatus}</td>
                             {
-                                myProduct.soldStatus === 'avialable' ? <td><button onClick={() => handleAdvertise(myProduct)} className='btn btn-outline btn-xs'>Advertise</button></td> : <td><button className='btn btn-outline btn-xs ' disabled>Advertised</button></td>
+                                myProduct.soldStatus === 'sold' ? <td><button className='btn btn-outline btn-xs ' disabled>Advertise</button></td> :
+
+                                    <td><button onClick={() => handleAdvertise(myProduct)} className='btn btn-outline btn-xs'>Advertise</button></td>
+
+
+
                             }
-                            <td><button className='btn btn-outline btn-xs'>Delete</button></td>
+                            <td><button onClick={() => handleDelete(myProduct._id)} className='btn btn-outline btn-xs'>Delete</button></td>
                         </tr>)
                     }
 
                 </tbody>
-            </table>
-        </div>
+            </table >
+        </div >
     );
 };
 
