96b0769cc50e36e01d3ab6f7ab4ed65d7e2c42eb
Rayhan-Rony <jobayerahmedrony@gmail.com>
2022-11-30 03:47:38 +0600

    added all user page

--- src/Pages/Dashboard/AllBuyers.js/AllBuyers.js
+++ src/Pages/Dashboard/AllBuyers.js/AllBuyers.js
@@ -1,10 +1,55 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 
 const AllBuyers = () => {
+    const { data: users = [] } = useQuery({
+        queryKey: ['user'],
+        queryFn: () => fetch('http://localhost:5000/user')
+            .then(res => res.json())
+
+    })
     return (
-        <div>
-            <h1>Buyers</h1>
-        </div>
+        <div className="overflow-x-auto min-h-screen">
+            <table className="table w-full">
+
+                <thead>
+                    <tr>
+
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Action</th>
+
+
+
+                    </tr>
+                </thead>
+                <tbody>
+
+
+
+                    {
+                        users.map(seller => <tr key={seller._id} className="hover">
+
+                            <td>{seller.name}</td>
+                            <td>{seller.email}</td>
+                            <td><button className='btn btn-outline btn-xs'>Delete</button></td>
+
+
+                            {/* {
+                                myProduct.soldStatus === 'sold' ? <td><button className='btn btn-outline btn-xs ' disabled>Advertise</button></td> :
+
+                                    <td><button onClick={() => handleAdvertise(myProduct)} className='btn btn-outline btn-xs'>Advertise</button></td>
+
+
+
+                            } */}
+                            {/* <td><button onClick={() => handleDelete(myProduct._id)} className='btn btn-outline btn-xs'>Delete</button></td> */}
+                        </tr>)
+                    }
+
+                </tbody>
+            </table >
+        </div >
     );
 };
 
